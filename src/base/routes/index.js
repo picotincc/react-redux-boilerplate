cf366cf7435e5ac51619805a2470cd9fb53d5a59
maybeShuo <546537825@qq.com>
2017-01-23 18:58:03 +0800

    Modify route.

--- src/base/routes/index.js
+++ src/base/routes/index.js
@@ -8,35 +8,13 @@ const home = (location, callback) => {
 	}, 'home');
 };
 
-const search = (location, callback) => {
-	require.ensure([], require => {
-		callback(null, require('search/containers/SearchContainer.js').default);
-	}, 'search');
-};
-
 const course = (location, callback) => {
 	require.ensure([], require => {
 		callback(null, require('course/containers/CourseContainer.js').default);
 	}, 'course');
 };
 
-const user = (location, callback) => {
-	require.ensure([], require => {
-		callback(null, require('user/containers/UserContainer.js').default);
-	}, 'user');
-};
 
-const order = (location, callback) => {
-	require.ensure([], require => {
-		callback(null, require('order/containers/OrderContainer.js').default);
-	}, 'order');
-};
-
-const myCourse = (location, callback) => {
-	require.ensure([], require => {
-		callback(null, require('mycourse/containers/MyCourseContainer.js').default);
-	}, 'mycourse');
-};
 
 
 const routes = (
@@ -44,11 +22,7 @@ const routes = (
 		<Route path='/' component={App}>
 			<IndexRoute getComponent={home} />
             <Route path='home' getComponent={home} />
-			<Route path='search' getComponent={search} />
-			<Route path='course' getComponent={course} />
-			<Route path='user' getComponent={user} />
-			<Route path='order' getComponent={order} />
-			<Route path='mycourse' getComponent={myCourse} />
+						<Route path='course' getComponent={course} />
 		</Route>
     </div>
 );
